a1d9172c4423f6dbdf0c85a82247d3dad15b5353
Julien Creach <julien.creach@pm.me>
2023-06-28 17:41:00 +0200

    first version

---
+++ SimpleAuth/Controllers/AuthenticationController.cs
@@ -0,0 +1,55 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using MockMyAuth.Models;
+
+namespace MockMyAuth.Controllers
+{
+    [ApiController, Route("api/[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public AuthenticationController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        [HttpGet("login")]
+        public ActionResult<string> GetToken(string username, string password)
+        {
+            if(string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+                return BadRequest();
+
+            return Ok(CreateToken(new UserDto {
+                Username = username,
+                Password = password
+            }));
+        }
+
+        private string CreateToken(UserDto user)
+        {
+            List<Claim> claims = new()
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var toto = _configuration.GetSection("AppSettings:Token").Value!;
+            SymmetricSecurityKey key = new(
+                Encoding.UTF8.GetBytes(toto));
+
+            SigningCredentials cred = new(key, SecurityAlgorithms.HmacSha512Signature);
+
+            JwtSecurityToken token = new(
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: cred
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+    }
+}
---
+++ SimpleAuth/Models/UserDto.cs
@@ -0,0 +1,8 @@
+namespace MockMyAuth.Models
+{
+    public class UserDto
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
